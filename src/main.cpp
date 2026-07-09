ee386b906d34581f55b7f901631b08129e5abda2
Kacper Kozlowski <kacperkozlow10@gmail.com>
2026-07-09 17:00:55 +0200

    roi mask

--- src/main.cpp
+++ src/main.cpp
@@ -14,24 +14,41 @@ int main(){
         return -1;
     }
 
-    cv::Mat img, firstFrame, imgGray, imgBlur, imgDiff;
+    cv::Mat img, imgGray, imgBlur, imgDiff, roiMASK;
+    cap.read(img);
+    
     cv::Ptr<cv::BackgroundSubtractorMOG2> sub = cv::createBackgroundSubtractorMOG2(500, 50, true);
+    cv::Mat kernelNoise = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3,3));
+    cv::Mat kernelCars = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(7,7));
+
+    cv::Mat mask = cv::Mat::zeros(img.size(), CV_8UC1);
+    std::vector<cv::Point> road = {
+        {0,745},
+        {0,1080},
+        {1690,1080},
+        {1780,750},
+        {1780,400},
+        {1080,400},
+        {750,0},
+        {500,0},
+        {435,300}
+    };
+
+    cv::fillPoly(mask, std::vector<std::vector<cv::Point>>{road}, cv::Scalar(255));
     
     while (true) {
-        cap.read(img);
 
-        if (img.empty()) {
+        if (!cap.read(img)) {
             std::cout << "video has ended" << std::endl;
             break;
         }
         cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
         cv::GaussianBlur(imgGray, imgBlur, cv::Size(3,3),0);
-
+        imgBlur.setTo(0, mask == 0);
         sub->apply(imgBlur, imgDiff);
-        cv::threshold(imgDiff, imgDiff, 130, 255, cv::THRESH_BINARY);
-        cv::Mat kernelNoise = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3,3));
-        cv::erode(imgDiff, imgDiff, kernelNoise);
-        cv::Mat kernelCars = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(21,21));
+
+        cv::threshold(imgDiff, imgDiff, 127, 255, cv::THRESH_BINARY);
+        cv::morphologyEx(imgDiff, imgDiff, cv::MORPH_OPEN,kernelNoise);
         cv::morphologyEx(imgDiff, imgDiff, cv::MORPH_CLOSE, kernelCars);
                 
         std::vector<std::vector<cv::Point>> contours;
@@ -40,9 +57,15 @@ int main(){
             if (cv::contourArea(contour) < 700) {
                 continue;
             }
+            cv::drawContours(img, contours, -1, cv::Scalar(255, 0, 0), 2);
             cv::Rect boundingRect = cv::boundingRect(contour);
             cv::rectangle(img, boundingRect, cv::Scalar(0, 255, 0), 2);
         }
+        img.copyTo(roiMASK, mask);
+
+        cv::imshow("ROI view", roiMASK);
+        cv::imshow("Motion", imgDiff);
+        cv::imshow("Mask", mask);
         cv::imshow("Image", img);
 
 
